a9c71075e97807659b2847839fea40c59622bce8
Petter Hopp <Petter.Hopp@vetinst.no>
2022-01-27 12:45:44 +0100

    feat: find_accepted_code now covers combinations of up to 3 variables

--- tests/testthat/test_find_accepted_code.R
+++ tests/testthat/test_find_accepted_code.R
@@ -5,11 +5,16 @@ test_that("find accepted code", {
   # Generate test data
   accepted_codes <- cbind(as.data.frame(rep(2021, 6)),
                           as.data.frame(rep("ok_gris_virus", 6)),
+                          as.data.frame(rep("0200102", 6)),
                           as.data.frame(rep("artkode", 6)),
                           as.data.frame(c("0310010100", "0310010100%", "0310010100",
-                                          "0310010100", "0310010100%", "0310010100")))
-  colnames(accepted_codes) <- c("aar", "program", "var1", "verdi1")
+                                          "0310010100", "0310010100%", "0310010100")),
+                          as.data.frame(rep(NA, 6)),
+                          as.data.frame(rep(NA, 6)),
+                          as.data.frame(rep(NA, 6)),
+                          as.data.frame(rep(NA, 6)))
+  colnames(accepted_codes) <- c("aar", "program", "hensikt", "var1", "verdi1", "var2", "verdi2", "var3", "verdi3")
 
-  expect_equal(find_accepted_code(data = accepted_codes, purpose = "ok_gris_virus", codetype = "artkode"),
-                    c("0310010100", "0310010100%"))
+  expect_equal(find_accepted_code(data = accepted_codes, purpose = "ok_gris_virus", code_variable = "artkode"),
+                    c("0200102-0310010100", "0200102-0310010100%"))
   })

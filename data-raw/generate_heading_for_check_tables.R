c2501465b2ce39dabbf6c2c4be26d1624784a545
Petter Hopp <Petter.Hopp@vetinst.no>
2022-02-10 10:54:16 +0100

    refactor: source tables for heading_for_check_tables are saved at "OKprogrammer"

--- data-raw/generate_heading_for_check_tables.R
+++ data-raw/generate_heading_for_check_tables.R
@@ -1,9 +1,12 @@
 ## code to prepare `heading_for_check_tables` dataset
 
-heading_for_check_tables <- openxlsx::read.xlsx("./data-raw/heading_for_check_tables.xlsx")
+heading_for_check_tables <- openxlsx::read.xlsx(paste0(NVIdb::set_dir_NVI("OKprogrammer"), "OKstatistikkApp/StotteData/heading_for_check_tables.xlsx"))
 
-write.csv2(heading_for_check_tables, file = "./data-raw/heading_for_check_tables.csv", row.names = FALSE, fileEncoding = "UTF-8")
+# Save as csv that can be loaded if package is not updated
+write.csv2(heading_for_check_tables, 
+           file = paste0(NVIdb::set_dir_NVI("OKprogrammer"), "OKstatistikkApp/StotteData/heading_for_check_tables.csv"), 
+           row.names = FALSE, 
+           fileEncoding = "UTF-8")
 
-heading_for_check_tables <- read.csv2(file = "./data-raw/heading_for_check_tables.csv", fileEncoding = "UTF-8")
-
+# Save in package
 usethis::use_data(heading_for_check_tables, overwrite = TRUE)

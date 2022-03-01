e30176d519a4dbf23b05c2a5d0d57fa787ee448b
Petter Hopp <Petter.Hopp@vetinst.no>
2022-03-01 15:50:13 +0100

    style: generate_headings_for_check_tables.R is styled

--- data-raw/generate_heading_for_check_tables.R
+++ data-raw/generate_heading_for_check_tables.R
@@ -3,9 +3,9 @@
 heading_for_check_tables <- openxlsx::read.xlsx(paste0(NVIdb::set_dir_NVI("OKprogrammer"), "OKstatistikkApp/StotteData/heading_for_check_tables.xlsx"))
 
 # Save as csv that can be loaded if package is not updated
-write.csv2(heading_for_check_tables, 
-           file = paste0(NVIdb::set_dir_NVI("OKprogrammer"), "OKstatistikkApp/StotteData/heading_for_check_tables.csv"), 
-           row.names = FALSE, 
+write.csv2(heading_for_check_tables,
+           file = paste0(NVIdb::set_dir_NVI("OKprogrammer"), "OKstatistikkApp/StotteData/heading_for_check_tables.csv"),
+           row.names = FALSE,
            fileEncoding = "UTF-8")
 
 # Save in package
@@ -13,4 +13,3 @@ usethis::use_data(heading_for_check_tables, overwrite = TRUE)
 
 
 rm(heading_for_check_tables)
-

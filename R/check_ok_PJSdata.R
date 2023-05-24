2826b033c95e06abafd307b4e8b96f698c4c2bfe
Petter Hopp <petter.hopp@vetinst.no>
2023-05-24 21:20:48 +0200

    doc: improved help for check_ok_PJSdata Introduced links to other functions.

--- R/check_ok_PJSdata.R
+++ R/check_ok_PJSdata.R
@@ -1,50 +1,54 @@
 #' @title Checks the standard output data frame with the OK selection
 #' @description Standard checks of PJSdata by checking for illegal or rare
 #'     combinations of values in variables in PJSdata.
-#'     \code{check_ok_PJSdata} is a wrapper for \code{NVIbatch::output_rendered}.
+#'     \code{check_ok_PJSdata} is a wrapper for
+#'     \ifelse{html}{\code{\link[NVIbatch:output_rendered]{NVIbatch::output_rendered}}}{\code{NVIbatch::output_rendered}}.
 #'
 #' @details Gives tables listing journals with mistakes, or probable mistakes.
 #'     These should be further checked and eventually reported to the laboratory
 #'     in charge of correcting the information in the journal.
 #'
 #' The check must be performed on a data frame with standardised
-#'     column names. This is ensured by using \code{data(OKplan::OK_column_standards)}
+#'     column names. This is ensured by using
+#'     \ifelse{html}{\code{\link[OKplan:OK_column_standards]{data(OKplan::OK_column_standards)}}}{\code{data(OKplan::OK_column_standards)}}
 #'     to standardise the column names.
 #'
 #' The default behaviour is to display the resulting html-file in the
 #'     browser. To save the result in a permanent file, use a permanent
-#'     directory as input to \code{output_dir = }. The resulting file
+#'     directory as input to \code{output_dir}. The resulting file
 #'     can also be sent by email by using additional arguments, see
-#'     \code{NVIbatch::output_rendered}.
+#'     \ifelse{html}{\code{\link[NVIbatch:output_rendered]{NVIbatch::output_rendered}}}{\code{NVIbatch::output_rendered}}.
 #'
 #' If a check don't find any mistakes that should be reported, there is not
 #'     produced any table for that check.
 #'
-#' @param input \[\code{character(1)}\]\cr
-#' The path to the rmarkdown document with the checks.Defaults to 
+#' @param input [\code{character(1)}]\cr
+#' The path to the rmarkdown document with the checks.Defaults to
 #'     "check_ok_PJSdata.Rmd" in the \code{OKplan}.
-#' @param output_file \[\code{character(1)}\]\cr
+#' @param output_file [\code{character(1)}]\cr
 #' The name of the output file.
-#' @param output_dir \[\code{character(1)}\]\cr
+#' @param output_dir [\code{character(1)}]\cr
 #' The directory to save the output file. Defaults to \code{NULL}.
-#' @param data \[\code{data.frame}\]\cr
+#' @param data [\code{data.frame}]\cr
 #' The table with data describing the selection for a OK programme.
-#' @param purpose \[\code{character(1)}\]\cr
+#' @param purpose [\code{character(1)}]\cr
 #' String with descriptive text to be used in file name and heading of the report.
-#' @param aar \[\code{numeric(1)}\]\cr
+#' @param aar [\code{numeric(1)}]\cr
 #' The year for which the selection is planned. Defaults to previous year.
-#' @param accept \[\code{character(1)}\]\cr
-#' How strict should the check of code combinations be. Defaults to 
+#' @param accept [\code{character(1)}]\cr
+#' How strict should the check of code combinations be. Defaults to
 #'     "accept_always", i.e. the most strict check.
-#' @param display \[\code{logical(1)} | \code{character(1)}\]\cr
+#' @param display [\code{logical(1)} | \code{character(1)}]\cr
 #' Set "browser" for the default browser or "viewer" for the R studio
-#'      viewer. `TRUE` equals "browser". If `FALSE`, don't display
+#'      viewer. \code{TRUE} equals "browser". If \code{FALSE}, don't display
 #'      the results file. Defaults to "browser".
-#' @param \dots Other arguments to be passed to `NVIbatch::output_rendered`.
+#' @param \dots Other arguments to be passed to
+#'     \ifelse{html}{\code{\link[NVIbatch:output_rendered]{NVIbatch::output_rendered}}}{\code{NVIbatch::output_rendered}}.
 #'
 #' @return Generates an html-file with the results of the checks to be displayed in the browser.
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
+#' @noMd
 #' @export
 #' @examples
 #' \dontrun{
